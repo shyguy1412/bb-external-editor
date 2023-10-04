7b62470a66c5f0fbadd513d1a31bd957a825674d
Nils Ramstöck <nilsramstoeck@gmail.com>
2023-10-04 10:15:35 +0200

    init project and add esbuild

---
+++ build.mjs
@@ -0,0 +1,24 @@
+import { context } from 'esbuild';
+import BitburnerPlugin from 'esbuild-bitburner-plugin';
+
+
+const WATCH = process.argv.includes('--watch');
+
+const ctx = await context({
+  entryPoints: await glob('./servers/**/*.{js,jsx,ts,tsx}'),
+  bundle: true,
+  plugins: [BitburnerPlugin],
+  outbase: "./servers",
+  minify: !WATCH,
+  format: 'esm',
+  platform: 'node',
+  define: WATCH ? undefined : {
+    'process.env.NODE_ENV': "'production'",
+  },
+  logLevel: 'info'
+});
+
+await ctx.rebuild();
+
+if (WATCH) ctx.watch();
+else ctx.dispose();
